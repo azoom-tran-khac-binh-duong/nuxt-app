927408cf6948d36ffcb4ef6fc9ce3397e8998ca0
Tran Khac Binh Duong <tran.khac.binh.duong@azoom.jp>
2024-10-24 16:15:19 +0700

    :thumbsup: Setup drizzle db

---
+++ db/seed.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import fs from 'fs';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { usersTable } from './schema/user';
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+  const users = JSON.parse(fs.readFileSync('./db/data/users.json', 'utf8'));
+  await db.insert(usersTable).values(users);
+  console.log('--- Seeded users ---');
+}
+
+main();
