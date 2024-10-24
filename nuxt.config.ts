1252dd23894d273ae3fbb9a5b2e1a04f6f19f7dc
Tran Khac Binh Duong <tran.khac.binh.duong@azoom.jp>
2024-10-24 16:27:36 +0700

    :thumbsup: Setup primevue ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,25 @@
+import Aura from '@primevue/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
-  }
+  },
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    autoImport: false,
+    options: {
+      ripple: true,
+      theme: {
+        preset: Aura
+      }
+    }
+  },
+  css: [
+    'primeicons/primeicons.css'
+  ]
 })
